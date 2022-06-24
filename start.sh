0ea8ed7ce995db84f841a060b8ea2322400b584d
Dito Windyaksa <morpig2@gmail.com>
2022-06-25 03:47:08 +0700

    create /dev/net

--- start.sh
+++ start.sh
@@ -2,6 +2,14 @@
 
 PID=$!
 
+if [[ ! -d /dev/net ]]; then
+  mkdir -p /dev/net
+fi
+
+if [[ ! -c /dev/net/tun ]]; then
+  mknod /dev/net/tun c 10 200
+fi
+
 /app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
 /app/tailscale up --authkey=${TAILSCALE_AUTH_KEY}
 
