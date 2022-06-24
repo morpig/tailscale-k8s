fa94d7067e97f3d8da9f1bc29c957cab68d9b502
Dito Windyaksa <morpig2@gmail.com>
2022-06-25 03:24:02 +0700

    add github actions, clean docker and fix startup script

--- Dockerfile
+++ Dockerfile
@@ -7,9 +7,6 @@ COPY . ./
 RUN apk update && apk add ca-certificates iptables ip6tables && rm -rf /var/cache/apk/*
 
 # Copy binary to production image
-COPY /app/start.sh /app/start.sh
-COPY /app/tailscaled /app/tailscaled
-COPY /app/tailscale /app/tailscale
 RUN mkdir -p /var/run/tailscale /var/cache/tailscale /var/lib/tailscale
 
 # Run on container startup.
--- start.sh
+++ start.sh
@@ -1,5 +1,22 @@
+FROM alpine:latest
+WORKDIR /app
+ENV TSFILE=tailscale_1.26.1_amd64.tgz
+RUN wget https://pkgs.tailscale.com/stable/${TSFILE} && tar xzf ${TSFILE} --strip-components=1
+COPY . ./
+
+RUN apk update && apk add ca-certificates iptables ip6tables && rm -rf /var/cache/apk/*
+
+# Copy binary to production image
+RUN mkdir -p /var/run/tailscale /var/cache/tailscale /var/lib/tailscale
+
+# Run on container startup.
+CMD ["/app/start.sh"]
+root@sin-leaseweb-ingest:~/tailscale-k8s# cat start.sh 
 #!/bin/sh
 
+PID=$!
+
 /app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
-/app/tailscale up --authkey=${TAILSCALE_AUTHKEY}
-/app/my-app
+/app/tailscale up --authkey=${TAILSCALE_AUTH_KEY}
+
+wait ${PID}
